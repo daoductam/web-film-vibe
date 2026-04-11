b47c44fb1abfc5e3a6fc234d09aeb96facf1516f
Dao Duc Tam <ddt@gmail.com>
2026-04-12 01:46:25 +0700

    fix: allow public key retrieval for mysql connection

--- web-film-android/app/src/main/java/com/tamdao/cinestream/core/util/Constants.kt
+++ web-film-android/app/src/main/java/com/tamdao/cinestream/core/util/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     //const val BASE_URL = "http://10.0.2.2:8081/api/"
     
     // IP máy tính trong mạng LAN (Cập nhật theo ipconfig)
-    const val BASE_URL = "http://10.20.7.147:8081/api/"
+    const val BASE_URL = "http://160.191.243.54:8081/api/"
 
     const val DATABASE_NAME = "cine_stream_db"
 }
